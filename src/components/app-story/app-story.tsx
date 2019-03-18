159dc40d4dad424bb6c5ffaf58c03649bedf4846
Amoroth <Amoroth36@gmail.com>
2019-03-18 23:06:48 +0100

    links to author page and comments on every story

--- src/components/app-story/app-story.tsx
+++ src/components/app-story/app-story.tsx
@@ -19,12 +19,12 @@ export class AppStory {
     return (
       <div>
         <p>Id: {this.storyObject.id}</p>
-        <p>By: {this.storyObject.by}</p>
+        <p>By: <stencil-route-link url={`/user/${this.storyObject.by}`}>{this.storyObject.by}</stencil-route-link></p>
         <p>Title: {this.storyObject.title}</p>
         <p>Url: {this.storyObject.url}</p>
         <p>Time: {new Date(this.storyObject.time).toString()}</p>
         <p>Score: {this.storyObject.score}</p>
-        <p>Comments: {this.storyObject.descendants}</p>
+        <p>Comments: <stencil-route-link url={`/item/${this.storyId}`}>{this.storyObject.descendants}</stencil-route-link></p>
       </div>
     )
   }
