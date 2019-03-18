fb968ce8c5e3abd082919797429c56ab07d5e7f5
Amoroth <Amoroth36@gmail.com>
2019-03-18 23:49:01 +0100

    show only url domain

--- src/components/app-story/app-story.tsx
+++ src/components/app-story/app-story.tsx
@@ -15,13 +15,15 @@ export class AppStory {
     this.storyObject = jsonResponse
   }
 
+  truncateURL = () => this.storyObject.url.match(/^(?:https?:\/\/)?(?:www\.)?([^:\/\n]+)/i)[1]
+
   render() {
     return (
       <div>
         <p>Id: {this.storyObject.id}</p>
         <p>By: <stencil-route-link url={`/user/${this.storyObject.by}`}>{this.storyObject.by}</stencil-route-link></p>
         <p>Title: {this.storyObject.title}</p>
-        <p>Url: {this.storyObject.url}</p>
+        <p>Url: {this.truncateURL()}</p>
         <p>Time: {new Date(this.storyObject.time).toString()}</p>
         <p>Score: {this.storyObject.score}</p>
         <p>Comments: <stencil-route-link url={`/item/${this.storyId}`}>{this.storyObject.descendants}</stencil-route-link></p>
