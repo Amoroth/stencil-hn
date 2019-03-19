567f91edf00ac980146c180a635bc9e063325508
Amoroth <Amoroth36@gmail.com>
2019-03-20 00:17:48 +0100

    story structure/changed to li/time formating

--- src/components/app-story/app-story.tsx
+++ src/components/app-story/app-story.tsx
@@ -1,33 +1,42 @@
 import { Component, Prop, State } from '@stencil/core'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
 import { IStory } from '../../global/types'
 
 @Component({
-  tag: 'app-story'
+  tag: 'app-story',
+  styleUrl: 'app-story.css',
+  shadow: true
 })
 export class AppStory {
   @Prop() storyId!: number
 
   @State() storyObject: IStory
+  @State() relativeTime: string
 
   async componentWillLoad() {
     const response = await fetch(`https://hacker-news.firebaseio.com/v0/item/${this.storyId}.json?print=pretty`)
     const jsonResponse: IStory = await response.json()
     this.storyObject = jsonResponse
+    dayjs.extend(relativeTime)
+    this.relativeTime = dayjs(jsonResponse.time * 1000).fromNow()
   }
 
   truncateURL = () => this.storyObject.url.match(/^(?:https?:\/\/)?(?:www\.)?([^:\/\n]+)/i)[1]
 
   render() {
     return (
-      <div>
-        <p>Id: {this.storyObject.id}</p>
-        <p>By: <stencil-route-link url={`/user/${this.storyObject.by}`}>{this.storyObject.by}</stencil-route-link></p>
-        <p>Title: {this.storyObject.title}</p>
-        <p>Url: {this.storyObject.url ? this.truncateURL() : null}</p>
-        <p>Time: {new Date(this.storyObject.time).toString()}</p>
-        <p>Score: {this.storyObject.score}</p>
-        <p>Comments: <stencil-route-link url={`/item/${this.storyId}`}>{this.storyObject.descendants}</stencil-route-link></p>
-      </div>
+      <li>
+        <span>{this.storyObject.score}</span>
+        <span>
+          <a href={this.storyObject.url || `/item/${this.storyId}`}>{this.storyObject.title}</a>
+          {this.storyObject.url ? <span>({this.truncateURL()})</span> : null}
+        </span>
+        <br />
+        <span>by <stencil-route-link url={`/user/${this.storyObject.by}`}>{this.storyObject.by}</stencil-route-link></span>
+        <span> {this.relativeTime}</span>
+        <span> | Comments: <stencil-route-link url={`/item/${this.storyId}`}>{this.storyObject.descendants}</stencil-route-link></span>
+      </li>
     )
   }
 }
