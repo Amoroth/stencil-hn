f627fb0bdcc7bade3dec1df3588b2e82f06d87ac
Amoroth <Amoroth36@gmail.com>
2019-03-20 15:22:38 +0100

    app-item structure

--- src/components/app-item/app-item.tsx
+++ src/components/app-item/app-item.tsx
@@ -1,5 +1,9 @@
-import { Component, Prop } from '@stencil/core'
+import { Component, Prop, State } from '@stencil/core'
 import { MatchResults } from '@stencil/router'
+import { IStory } from '../../global/types'
+import { truncateURL } from '../../global/app'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
 
 @Component({
   tag: 'app-item'
@@ -7,10 +11,29 @@ import { MatchResults } from '@stencil/router'
 export class AppItem {
   @Prop() match: MatchResults
 
+  @State() storyObject: IStory
+  @State() relativeTime: string
+
+  async componentWillLoad() {
+    const response: IStory = await fetch(`https://hacker-news.firebaseio.com/v0/item/${this.match.params.id}.json?print=pretty`)
+      .then(res => res.json())
+    this.storyObject = response
+    dayjs.extend(relativeTime)
+    this.relativeTime = dayjs(response.time * 1000).fromNow()
+  }
+
   render() {
     return (
       <div>
-        <p>Item {this.match.params.id}</p>
+        <h2>
+          <a href={this.storyObject.url}>{this.storyObject.title}</a>
+          <span> ({truncateURL(this.storyObject.url)})</span>
+        </h2>
+        <br />
+        <span class="meta">{this.storyObject.score} points |</span>
+        <span> by <stencil-route-link>{this.storyObject.by}</stencil-route-link> |</span>
+        <span> {this.relativeTime}</span>
+        {/* <app-comment /> */}
       </div>
     )
   }
